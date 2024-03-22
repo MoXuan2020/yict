229d27d0391bb0f43729f952133756939be796f4
MoXuan2020 <2809603169@qq.com>
2024-03-22 10:36:25 +0800

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,60 @@
+basicTypeList = [int, float, str]
+basicContainerList = [list]
+
+
+def toDict(src):
+    tarDict = {
+        "_m_": src.__class__.__module__,
+        "_n_": src.__class__.__name__,
+    }
+    srcDict = vars(src)
+    for key in srcDict:
+        value = srcDict[key]
+        valueType = type(value)
+        if valueType in basicTypeList:
+            tarDict[key] = value
+        elif valueType in basicContainerList:
+            tarDict[key] = toListDict(value)
+        else:
+            tarDict[key] = toDict(value)
+    return tarDict
+
+
+def toListDict(oldList):
+    newList = []
+    for value in oldList:
+        valueType = type(value)
+        if valueType in basicTypeList:
+            newList.append(value)
+        elif valueType in basicContainerList:
+            newList.append(toListDict(value))
+        else:
+            newList.append(toDict(value))
+    return newList
+
+
+def fromDict(srcDict):
+    tarObj = getattr(__import__(srcDict["_m_"], fromlist=[""]), srcDict["_n_"])()
+    for key in srcDict:
+        if key not in ["_m_", "_n_"]:
+            value = srcDict[key]
+            valueType = type(value)
+            if valueType in basicTypeList:
+                setattr(tarObj, key, value)
+            elif valueType in basicContainerList:
+                setattr(tarObj, key, fromListDict(value))
+            else:
+                setattr(tarObj, key, fromDict(value))
+    return tarObj
+
+
+def fromListDict(oldList):
+    newList = []
+    for value in oldList:
+        if type(value) in basicTypeList:
+            newList.append(value)
+        elif type(value) in basicContainerList:
+            newList.append(fromListDict(value))
+        else:
+            newList.append(fromDict(value))
+    return newList
